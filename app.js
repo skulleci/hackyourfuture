9fc1c2e4cdb0fd2d591166197518955ce8ad92fa
skulleci <77619047+skulleci@users.noreply.github.com>
2021-07-22 00:38:14 +0200

    new

--- app.js
+++ app.js
@@ -33,20 +33,14 @@ const desc = document.querySelector(".desc");
 const temp = document.querySelector(".temp");
 const icon = document.querySelector('.icon');
 
-window.addEventListener('load', () => {
-    const getCitys = () => {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=ce27b58ce285f0299f0de1cb104d4c9c').then(function (response) {
-      return response.json().then(function (response) {
-        console.log(response)
-        submitButton.addEventListener('click', async () => {
-          const { data } = await getCitys();
-          console.log(data);
-          names.innerHTML = data.name;
-          temp.innerHTML = Math.floor(`${data.main.temp}`- 273.15) +" " + "C";
-          desc.innerHTML = data.weather[0].description;
-          icon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-        });
-      });
-    });
-    }   
+submitButton.addEventListener('click', () => {
+  fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=ce27b58ce285f0299f0de1cb104d4c9c').then(response => {
+  return response.json();
+  }).then((data) => {
+  console.log(data);
+    names.innerHTML = data.name;
+    temp.innerHTML = Math.floor(`${data.main.temp}`- 273.15) +" " + "C";
+    desc.innerHTML = data.weather[0].description;
+    icon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+  }); 
 });
